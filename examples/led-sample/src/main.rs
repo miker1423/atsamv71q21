466c39077ac0cf62e88acd36737eabf73e8f6a9a
Miguel Pérez García <miguelpg_95@hotmail.com>
2020-09-26 09:01:39 -0500

    adding example

--- examples/led-sample/src/main.rs
+++ examples/led-sample/src/main.rs
@@ -18,10 +18,25 @@ fn main() -> ! {
 
     let port_a = p.PIOA;
     port_a.idr.write_with_zero(|w| w.p23().set_bit());
+    port_a.pudr.write_with_zero(|w| w.p23().set_bit());
     port_a.abcdsr.first().unwrap().modify(|_, w| w.p23().set_bit());
     port_a.abcdsr.last().unwrap().modify(|_, w| w.p23().set_bit());
 
+    let mut is_set = true;
 
+    loop {
+        if is_set {
+            port_a.codr.write_with_zero(|w| w.p23().set_bit());
+        } else {
+            port_a.sodr.write_with_zero(|w| w.p23().set_bit());
+        }
+        delay();
+        is_set = !is_set;
+    }
+}
 
-    loop {}
+fn delay(){
+    for _ in 0..10_000_00 {
+        cortex_m::asm::nop();
+    }
 }
