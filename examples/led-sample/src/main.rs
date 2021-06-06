b6aa472c8b95d63bf621bca0f51aceb695310318
Miguel Pérez García <miguelpg_95@hotmail.com>
2021-06-06 13:06:34 -0500

    Preping to publish crate

--- examples/led-sample/src/main.rs
+++ examples/led-sample/src/main.rs
@@ -17,19 +17,23 @@ fn main() -> ! {
     let watchdog_timer = p.WDT;
     watchdog_timer.mr.write(|w| w.wddis().set_bit());
 
-    let port_a = p.PIOA;
-    port_a.idr.write_with_zero(|w| w.p23().set_bit());
-    port_a.pudr.write_with_zero(|w| w.p23().set_bit());
+
+    let port_a = &p.PIOA;
+    unsafe {
+        port_a.idr.write_with_zero(|w| w.p23().set_bit());
+        port_a.pudr.write_with_zero(|w| w.p23().set_bit());
+    }
     port_a.abcdsr.first().unwrap().modify(|_, w| w.p23().set_bit());
     port_a.abcdsr.last().unwrap().modify(|_, w| w.p23().set_bit());
 
     let mut is_set = true;
 
     loop {
+        let value = port_a.odsr.read().bits();
         if is_set {
-            port_a.codr.write_with_zero(|w| w.p23().set_bit());
+            unsafe { port_a.codr.write_with_zero(|w| w.p23().set_bit()); }
         } else {
-            port_a.sodr.write_with_zero(|w| w.p23().set_bit());
+            unsafe { port_a.sodr.write_with_zero(|w| w.bits(value & (1 << 23))); }
         }
         delay();
         is_set = !is_set;
---
+++ readme.md
@@ -0,0 +1,15 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+
+# ATSAMV71Q21
+
+Peripheral Access Crate
+
+## Getting Started
+check out the following ressources to get started with embedded in Rust:
+* [Embedded Rust documentation](https://docs.rust-embedded.org)
+* [Embedded in Rust](http://blog.japaric.io)
+
+Examples can be found [here](examples).
+
+## Linker file
+A [linker file](memory.x) can be found in the repository.
